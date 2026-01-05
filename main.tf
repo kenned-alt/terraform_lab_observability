9d6c6dc637285edb95558e8a26a19c5615790e6f
John Kennedy <kenned10y@gmail.com>
2026-01-05 16:40:43 -0300

    fix: arquivo yml

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ terraform {
 provider "azurerm" {
   resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
-#  subscription_id = "e2192c0d-e4d5-4cb3-9241-bb1e1e771426"
-#  tenant_id = "c1f17d5e-f231-4f63-81a1-966179fc6eca"
+  subscription_id = "e2192c0d-e4d5-4cb3-9241-bb1e1e771426"
+  tenant_id = "c1f17d5e-f231-4f63-81a1-966179fc6eca"
 }
